28067fcd02168546fcb17709fa73d633ae75d581
Ya Zhuang <zhuangya@gmail.com>
2016-04-02 19:11:06 +0800

    add: url validation
    if you do not want to valid url, invoke jare like this:
    
    ```javascript
    
    jare('http://123.dev/456.png', {
      prefix: 'http://other.jare.io/?u=',
      check: false
    });
    ```

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 'use strict';
 
+const validUrl = require('valid-url');
+const assert = require('assert');
+
 module.exports = function jare (url, prefix) {
+  var check = true;
+
+  if (typeof prefix === 'object') {
+    let option = prefix;
+    prefix = option.prefix;
+    check = option.check;
+  }
+
+  if (check) {
+    assert.equal(validUrl.isUri(url), url, 'not a valid url');
+  }
+
   prefix = prefix || 'http://cf.jare.io/?u=';
   return prefix + encodeURIComponent(url);
+
 };
--- test.js
+++ test.js
@@ -1,10 +1,22 @@
 'use strict';
 
-const jare = require('./');
-const assert = require('assert');
+import test from 'ava';
+import jare from '.';
 
-console.log('it should add prefix for a url');
-assert.equal(jare('http://google.com/logo.gif'), 'http://cf.jare.io/?u=http%3A%2F%2Fgoogle.com%2Flogo.gif');
+test('it should add prefix for a url', t => {
+  t.is(jare('http://google.com/logo.gif'), 'http://cf.jare.io/?u=http%3A%2F%2Fgoogle.com%2Flogo.gif');
+});
 
-console.log('it should be able to specific another prefix');
-assert.equal(jare('http://google.com/logo.gif', 'http://xo.jare.io/?u='), 'http://xo.jare.io/?u=http%3A%2F%2Fgoogle.com%2Flogo.gif');
+test('it should be able to specific another prefix', t => {
+  t.is(jare('http://google.com/logo.gif', 'http://xo.jare.io/?u='), 'http://xo.jare.io/?u=http%3A%2F%2Fgoogle.com%2Flogo.gif');
+});
+
+test('it should only process with valid url', t => {
+  t.throws(_ => {
+    jare('there is no spoon');
+  }, /not a valid url/);
+});
+
+test('it should not valid url if option.check = false', t => {
+  t.is(jare('there is no spoon', { check: false }), 'http://cf.jare.io/?u=there%20is%20no%20spoon');
+});
